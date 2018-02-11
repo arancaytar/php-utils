94c3c2f3ec8ef9ee6e5447da2a2858de22a5f615
Aran <aran@ermarian.net>
2018-02-11 13:14:22 +0000

    Load jQuery from CDN.

--- ermarian/ermarian.tpl.php
+++ ermarian/ermarian.tpl.php
@@ -50,6 +50,10 @@
           href="css/print.css" />
 
     <?=$styles?>
+    <script
+      src="https://code.jquery.com/jquery-3.0.0.min.js"
+      integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0="
+      crossorigin="anonymous"></script>
     <?=$scripts?>
 
     <!-- /styles and scripts -->
